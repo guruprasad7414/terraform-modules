77488cb40c4405380862971f6a5cc5397f27f73e
adhig93 <81528871+adhig93@users.noreply.github.com>
2022-05-06 21:59:34 +0530

    Add files via upload

---
+++ modules/create_ec2/ec2.tf
@@ -0,0 +1,10 @@
+resource "aws_instance" "my-instance" {
+	ami = var.instance_ami
+	instance_type = var.instance_type
+	key_name = var.instance_key
+
+	tags = {
+		Name = var.instance_name
+	}
+	subnet_id = "subnet-087923e8bc24afc62"
+}
